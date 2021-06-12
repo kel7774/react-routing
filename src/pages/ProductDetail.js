fb74faacc3f0e543b907c814462fdb5633d2d5cd
Kelli Landry <landry.kel@gmail.com>
2021-06-12 11:00:19 -0500

    practice basic react-router-dom feature implementation

---
+++ src/pages/ProductDetail.js
@@ -0,0 +1,13 @@
+import { useParams } from 'react-router-dom'
+const ProductDetail = () => {
+  const params = useParams()
+  console.log(params.productId)
+  return (
+    <section>
+      <h1>Product Details Page</h1>
+      <p>{params.productId}</p>
+    </section>
+  )
+}
+
+export default ProductDetail
